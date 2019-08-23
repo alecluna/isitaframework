a0b60a2d3292a14455c47491ef79553489901672
Alec Luna <alecluna@csus.edu>
2019-08-23 12:08:49 -0700

    add react-spring to container

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import * as React from "react";
+import { useSpring, animated } from "react-spring";
 import Search from "./Components/Search";
 import "./Styles/container.css";
 import "./Styles/fonts.css";
 
 const App = (): any => {
+  const props = useSpring({ opacity: 1, from: { opacity: 0 } });
+
   return (
     <div className="container">
-      <h1 className="header">
-        Is it a <strong>framework</strong>?
-      </h1>
-      <Search />
+      <animated.div style={props}>
+        <h1 className="header">
+          Is it a <strong>framework</strong>?
+        </h1>
+        <Search />
+      </animated.div>
     </div>
   );
 };
