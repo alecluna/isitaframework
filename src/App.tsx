fc371e66f12f8e785b225f17dee81da600623958
Alec Luna <alecluna@csus.edu>
2019-08-26 14:48:51 -0700

    add body css and Toggle components, toggle works but needs to be styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
 import { useSpring, animated } from "react-spring";
+import DarkModeToggle from "./Components/DarkMode/DarkModeToggle";
 import Search from "./Components/Search";
 import "./Styles/container.css";
 import "./Styles/fonts.css";
+import "./Styles/darkmode.css";
 
 const App = (): any => {
   const props = useSpring({ opacity: 1, from: { opacity: 0 } });
 
   return (
     <div className="container">
+      <DarkModeToggle />
       <animated.div style={props}>
         <h1 className="header">
           Is it a <strong>framework</strong>?
